feaab3c613d789cb73b33d21e79cc7e1e70658fe
Ale <agallart@itba.edu.ar>
2022-10-23 12:08:04 -0300

    Descriptivos de la base de datos y nulos

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import pandas 
 
-## cargar data
+### cargar data ###
 filename = './tmdb_5000_movies.csv'
 data = pandas.read_csv(filename, header=0)
 
-## descriptivos del raw data set
+
+### Limpieza ###
+
+## descriptivos del raw data set, chequeo nulos y duplicados
 print(data.shape)
 print(data.head(10))
-data.info()  #se puede ver que hay nulos
-print(data.describe())
-
-#veo nulos
-print(data.isnull().sum())
 print(data.dtypes)
-#data.fillna('null',inplace= True) #el problema es que deberia remplazar los strings por un string y los numeros por numeros
-#print(data.isnull().sum())
+print(data.isnull().sum()) #hay nulos
+print(data.duplicated().sum())  #no hay duplicados
 
-## duplicados 
-print(data.duplicated().sum()) #no hya duplicados
+##nulos
+dataClean = data
+dataClean['homepage'].fillna('null',inplace= True) #ambos son strings no pasa nada si no tienen este campo
+dataClean['tagline'].fillna('null',inplace= True) 
+dataClean['overview'].fillna('null',inplace= True)
+#elimino las filas de los que tienen nulos y no son significativos, en este caso 2 filas de release_date y 1 de runtime
+dataClean.dropna(inplace= True)
+print(data1.isnull().sum())
 
 
-#agrego esto para pushear
+
