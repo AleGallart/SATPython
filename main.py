7d4e74a1927f2c76c1bbf2a0f05d6b1bbe1998ad
Ale <agallart@itba.edu.ar>
2022-10-23 10:56:15 -0300

    elimino y dejo solo la carga de datos

--- main.py
+++ main.py
@@ -4,20 +4,3 @@ import pandas
 filename = './tmdb_5000_movies.csv'
 data = pandas.read_csv(filename, header=0)
 
-## descriptivos del raw data set
-print(data.shape)
-print(data.head(10))
-data.info()  #se puede ver que hay nulos
-print(data.describe())
-
-#veo nulos
-print(data.isnull().sum())
-print(data.dtypes)
-#data.fillna('null',inplace= True) #el problema es que deberia remplazar los strings por un string y los numeros por numeros
-#print(data.isnull().sum())
-
-## duplicados 
-print(data.duplicated().sum()) #no hya duplicados
-
-
-#agrego esto para pushear
